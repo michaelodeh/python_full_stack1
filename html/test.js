0bb6b8a6b3c2b82d425e1a5e9ca82bbae2a219b9
michaelodeh <michael4dominion2@gmail.com>
2024-06-18 22:25:36 +0100

    Javascript Added

---
+++ html/test.js
@@ -0,0 +1,25 @@
+// var v4 = "John Doe";
+// V case sensitive
+// Cannot with a number or any symbols except _
+// space is 4bit like characters
+// console.log(v);
+// console.log(v);
+// console.log(v);
+// console.log(v);
+
+//var, const ,let
+
+// var v = 3
+// var v = 4
+// v = 5
+
+
+// let v = 3 
+// // let v = 4
+// v = 5
+
+const v =5
+// const v = 5
+// v = 3
+
+console.log(v);
